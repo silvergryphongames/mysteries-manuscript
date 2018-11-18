f18f63462e9a61396badb6de66eb349243dbb4c6
Ben Overmyer <manatrance@gmail.com>
2018-11-18 23:15:10 +0000

    Update markdown-style.rb

---
+++ markdown-style.rb
@@ -0,0 +1,10 @@
+# Markdown style rules
+all
+exclude_rule 'MD013'
+exclude_rule 'MD022'
+exclude_rule 'MD023'
+exclude_rule 'MD024'
+exclude_rule 'MD035'
+exclude_rule 'MD041'
+
+rule 'MD029', :style => 'ordered'
